24b074099730e656ea0640de3dbc51836291c922
Elias-Cereberg <elias-cereberg@outlook.com>
2018-11-18 18:39:25 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -2,33 +2,54 @@
 #include <fstream>
 
 /**
- * The program reads numbers from a file and prints the sum of them
+ * The program reads numbers from input file and prints the Sum of them
  * Example: main.exe ./tests/input.in
  * @param  argc [description]
  * @param  argv [description]
  * @return      [description]
  */
-int main(int argc, const char * argv[])
-{   
-    int a = 0;
-    int sum = 0;
-    //abdo är sexig
-    //test commit
-    
+int main(int argc, const char* argv[])
+{
+    float Sum = 0;
+    int Counter = 0;
+    float* dynArr;
+    std::string line;
     std::ifstream myReadFile;
-    
     myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
+
+    if (argc < 2) {
+        std::cout << "Invalid amount of parameters, did you pass the file correctly?" << std::endl;
+        exit(EXIT_FAILURE);
     }
+    while (myReadFile >> line) {
+        Counter += 1;
+    }
+
+    dynArr = new float[Counter];
+    myReadFile.clear();
+    myReadFile.seekg(0, myReadFile.beg);
+
+    for (int i = 0; i < Counter; i++) {
+        if (myReadFile >> dynArr[i]) {
+            Sum += dynArr[i];
+        }
+        else {
+            std::cout << "Invalid input data" << std::endl;
+            delete[] dynArr;
+            exit(EXIT_FAILURE);
+        }
+    }
+
     myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
+    float average = Sum / Counter;
+    float printableAverage = (float)((int)(average * 1000)) / 1000;
+   
+    for (int i = 0; i < Counter; i++) {
+        if (dynArr[i] > average)
+            std::cout << ((float)((int)(dynArr[i] * 1000)) / 1000) << " is bigger than the average which is " << printableAverage << std::endl;
+    }
+
+    delete[] dynArr;
+    system("PAUSE");
     return 0;
 }
-
